84066f65709fb8720a17fa65ae3548f7202c1854
nitin <nitin.nbu@gmail.com>
2020-08-15 02:52:50 -0400

    correction

--- sa/sa.py
+++ sa/sa.py
@@ -2,7 +2,7 @@ import boto3
 
 if __name__ == '__main__':
     session = boto3.Session(profile_name='sa')
-    session = boto3.Session(profile_name='sa')
     ec2 = session.resource('ec2')
+
     for i in ec2.instances.all():
         print(i)
